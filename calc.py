d7b381b6cce9e7d4abfa54655c98f2cf5b111544
Arbaaz Khan <crispyzingy@gmail.com>
2020-05-21 20:44:37 +0530

    added check for input

--- calc.py
+++ calc.py
@@ -20,17 +20,18 @@ print("1. Addition")
 print("2. Subtract\n")
 
 # Ask choice of the user
-choice = int(input("Enter choice(1/2): "))
+choice = input("Enter choice(1/2): ")
 
-# Take input from user
-x = float(input("\nEnter First Number: "))
-y = float(input("Enter Second Number: "))
+if choice in ("1", "2"):
+    # Take input from user
+    x = float(input("\nEnter First Number: "))
+    y = float(input("Enter Second Number: "))
 
-if choice == 1:
-    print(f"\n{x} + {y} = {calc(add, x, y)}")
+    if choice == "1":
+        print(f"\n{x} + {y} = {calc(add, x, y)}")
 
-elif choice == 2:
-    print(f"\n{x} - {y} = {calc(sub, x, y)}")
+    elif choice == "2":
+        print(f"\n{x} - {y} = {calc(sub, x, y)}")
 
 else:
     print("\nInvalid Input")
