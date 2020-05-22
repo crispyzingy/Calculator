5c4af11003e17050d11252335d922de4cd086785
Arbaaz Khan <crispyzingy@gmail.com>
2020-05-22 22:46:59 +0530

    added infinite menu and quit function

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 # A simple calculator that can add, subtract, multiply and divide
+import sys
 
 
 def calc(f, x, y):
@@ -25,31 +26,36 @@ def div(x, y):
     return x / y
 
 
-print("Select operation:")
-print("1. Addition")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Division\n")
+while True:
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Division")
+    print("5. Quit\n")
 
-# Ask choice of the user
-choice = input("Enter choice(1/2/3/4): ")
+    # Ask choice of the user
+    choice = input("Enter choice(1/2/3/4/5): ")
 
-if choice in ("1", "2", "3", "4"):
-    # Take input from user
-    x = float(input("\nFirst Number: "))
-    y = float(input("Second Number: "))
+    if choice in ("1", "2", "3", "4"):
+        # Take input from user
+        x = float(input("\nFirst Number: "))
+        y = float(input("Second Number: "))
 
-    if choice == "1":
-        print(f"\n{x} + {y} = {calc(add, x, y)}")
+        if choice == "1":
+            print(f"\n{x} + {y} = {calc(add, x, y)}\n")
 
-    elif choice == "2":
-        print(f"\n{x} - {y} = {calc(sub, x, y)}")
+        elif choice == "2":
+            print(f"\n{x} - {y} = {calc(sub, x, y)}\n")
 
-    elif choice == "3":
-        print(f"\n{x} * {y} = {calc(mul, x, y)}")
+        elif choice == "3":
+            print(f"\n{x} * {y} = {calc(mul, x, y)}\n")
 
-    elif choice == "4":
-        print(f"\n{x} / {y} = {calc(div, x, y)}")
+        elif choice == "4":
+            print(f"\n{x} / {y} = {calc(div, x, y)}\n")
 
-else:
-    print("\nInvalid Input")
+    elif choice == "5":
+        sys.exit()
+
+    else:
+        print("\nInvalid Input\n")
