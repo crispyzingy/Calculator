9a92ebceb0b27d3492651dce026e4cb807a23fb5
Arbaaz Khan <crispyzingy@gmail.com>
2020-05-23 23:26:38 +0530

    fixed infinite menu

--- calc.py
+++ calc.py
@@ -26,36 +26,55 @@ def div(x, y):
     return x / y
 
 
-while True:
-    print("Select Operation:")
-    print("1. Addition")
-    print("2. Subtract")
-    print("3. Multiply")
-    print("4. Division")
-    print("5. Quit\n")
-
-    # Ask choice of the user
-    choice = input("Enter choice(1/2/3/4/5): ")
-
-    if choice in ("1", "2", "3", "4"):
-        # Take input from user
-        x = float(input("\nFirst Number: "))
-        y = float(input("Second Number: "))
-
-        if choice == "1":
-            print(f"\n{x} + {y} = {calc(add, x, y)}\n")
-
-        elif choice == "2":
-            print(f"\n{x} - {y} = {calc(sub, x, y)}\n")
-
-        elif choice == "3":
-            print(f"\n{x} * {y} = {calc(mul, x, y)}\n")
-
-        elif choice == "4":
-            print(f"\n{x} / {y} = {calc(div, x, y)}\n")
-
-    elif choice == "5":
-        sys.exit("See you later!")
-
+def again():
+    calc_again = input(
+        "Do you want to calculate again? Please type Y for YES or N for NO. "
+    )
+    if calc_again.lower() == "y":
+        calculate()
+    elif calc_again.lower() == "n":
+        print("See you later.")
     else:
-        print("\nInvalid Input\n")
+        again()
+
+
+def calculate():
+    choices = ["1", "2", "3", "4"]
+    choice = "0"  # initiation
+    ready_to_quit = False
+    while not ready_to_quit and choice not in choices:
+        print("What would you like to do?")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Quit\n")
+
+        # Ask choice of the user
+        choice = input("Enter choice (1/2/3/4/5) and press enter: ")
+
+        if choice in choices:
+            # Take input from user
+            x = float(input("\nFirst Number: "))
+            y = float(input("Second Number: "))
+
+            if choice == "1":
+                print(f"\n{x} + {y} = {calc(add, x, y)}\n")
+
+            elif choice == "2":
+                print(f"\n{x} - {y} = {calc(sub, x, y)}\n")
+
+            elif choice == "3":
+                print(f"\n{x} * {y} = {calc(mul, x, y)}\n")
+
+            elif choice == "4":
+                print(f"\n{x} / {y} = {calc(div, x, y)}\n")
+
+        elif choice == "5":
+            ready_to_quit = True
+            sys.exit("See you later!")
+
+        again()
+
+
+menu()
