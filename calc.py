8d5b679d8e4305dca32a045578cd6128fbc468c4
Arbaaz Khan <crispyzingy@gmail.com>
2020-05-24 23:27:57 +0530

    Refactor done

--- calc.py
+++ calc.py
@@ -1,29 +1,4 @@
 # A simple calculator that can add, subtract, multiply and divide
-import sys
-
-
-def calc(f, x, y):
-    return f(x, y)
-
-
-# adds two numbers
-def add(x, y):
-    return x + y
-
-
-# subtracts two numbers
-def sub(x, y):
-    return x - y
-
-
-# subtracts two numbers
-def mul(x, y):
-    return x * y
-
-
-# subtracts two numbers
-def div(x, y):
-    return x / y
 
 
 def again():
@@ -39,42 +14,27 @@ def again():
 
 
 def calculate():
-    choices = ["1", "2", "3", "4"]
-    choice = "0"  # initiation
-    ready_to_quit = False
-    while not ready_to_quit and choice not in choices:
-        print("What would you like to do?")
-        print("1. Add")
-        print("2. Subtract")
-        print("3. Multiply")
-        print("4. Divide")
-        print("5. Quit\n")
+    # Take input from user
+    print("Press Enter Key after giving your input")
 
-        # Ask choice of the user
-        choice = input("Enter choice (1/2/3/4/5) and press enter: ")
+    num1 = float(input("First Number:\n"))
+    operator = input("Operator (+, -, *, /):\n")
+    num2 = float(input("Second Number:\n"))
 
-        if choice in choices:
-            # Take input from user
-            x = float(input("\nFirst Number: "))
-            y = float(input("Second Number: "))
+    out = None
 
-            if choice == "1":
-                print(f"\n{x} + {y} = {calc(add, x, y)}\n")
+    if operator == "+":
+        out = num1 + num2
+    elif operator == "-":
+        out = num1 - num2
+    elif operator == "*":
+        out = num1 * num2
+    elif operator == "/":
+        out = num1 / num2
 
-            elif choice == "2":
-                print(f"\n{x} - {y} = {calc(sub, x, y)}\n")
+    print(f"Answer: {out}")
 
-            elif choice == "3":
-                print(f"\n{x} * {y} = {calc(mul, x, y)}\n")
-
-            elif choice == "4":
-                print(f"\n{x} / {y} = {calc(div, x, y)}\n")
-
-        elif choice == "5":
-            ready_to_quit = True
-            sys.exit("See you later!")
-
-        again()
+    again()
 
 
 calculate()
